2c790897bdc26604e365c0ab34aaec93db892990
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2019-10-17 14:20:18 +0300

    .

--- lab2.pl
+++ lab2.pl
@@ -2,25 +2,27 @@
                                           /* Miestų schema */
 
 /**********************************************************************************************/
-/*                  100 km                     200 km                       30 km             */
-/*  Vilnius <---------------> Kaunas <------------------------> Klaipeda<--------->Palanga    */
-/*          <\                 />                        ___/>                                */
-/*            \               /                      ____/                                    */
+/*                  100 km                     200 km                    30 km                */
+/*  Vilnius ---------------> Kaunas ------------------------> Klaipeda--------->Palanga       */
+/*           \                 /                        ___/>                                 */
+/*            \               /                       ___/                                    */
 /*             \      140 km /            160 km ____/                                        */
 /*     220 km   \           /              ____/                                              */
 /*               \         /          ____/                                                   */
 /*                \       /      ___/                                                         */
-/*                 \>    </ <____/                                                            */
+/*                 \>    </ ____/                                                             */
 /*                  Siauliai                                                                  */
 /*                                                                                            */
 /**********************************************************************************************/
 
-% [1] Duotas miestus jungiančių kelių tinklas. 
-%Keliai vienakrypčiai, nesudarantys ciklų. 
-%Kiekvienas kelias turi savo ilgį. 
-%Ši informacija išreiškiama faktais kelias(Miestas1, Miestas2, Atstumas). 
-%Apibrėžkite predikatą „galima nuvažiuoti iš miesto X į miestą Y“:
-%nuvažiavus lygiai L kilometrų.
+/*
+[1] Duotas miestus jungiančių kelių tinklas. 
+    Keliai vienakrypčiai, nesudarantys ciklų. 
+    Kiekvienas kelias turi savo ilgį. 
+    Ši informacija išreiškiama faktais kelias(Miestas1, Miestas2, Atstumas). 
+[1.2] Apibrėžkite predikatą „galima nuvažiuoti iš miesto X į miestą Y“:
+    nuvažiavus lygiai L kilometrų.
+*/
 
 kelias(vilnius, kaunas, 100).
 kelias(vilnius, siauliai, 220).
@@ -30,29 +32,35 @@ kelias(siauliai, klaipeda, 160).
 kelias(klaipeda, palanga, 30).
 
 galima_nuvaziuoti(MiestasX, MiestasY, L):- kelias(MiestasX, MiestasY, L).
-galima_nuvaziuoti(MiestasX, MiestasY, L):- MiestasX \= MiestasY, kelias(MiestasX, MiestasT, KMXT), galima_nuvaziuoti(MiestasT, MiestasY, KMTY), L is KMTY + KMXT. 
+galima_nuvaziuoti(MiestasX, MiestasY, L):- 
+    MiestasX \= MiestasY, 
+    kelias(MiestasX, MiestasT, KMXT),
+    galima_nuvaziuoti(MiestasT, MiestasY, KMTY),
+    L is KMTY + KMXT. 
 
-% [2] Natūralieji skaičiai yra modeliuojami termais nul, s(nul), s(s(nul)),… 
-%(žr. paskaitos medžiagą). Apibrėžkite predikatus:
-%    „dviejų skaičių skirtumas lygus trečiajam skaičiui.“
+/*
+[6] Natūralieji skaičiai yra modeliuojami termais nul, s(nul), s(s(nul)),… 
+    (žr. paskaitos medžiagą). Apibrėžkite predikatus:
+[6.2] „dviejų skaičių skirtumas lygus trečiajam skaičiui.“
+*/
 
 % budas nr1
 diff(A, A, 0) :-true.
 diff(s(A),B,s(D)) :- diff(A,B,D).
 
 % budas nr2
-%diff(A, 0, Z) :- Z = A.
-%diff(s(A),s(B),Z) :- diff(A,B,Z).
+% diff(A, 0, Z) :- Z = A.
+% diff(s(A),s(B),Z) :- diff(A,B,Z).
 
-/*******************************************************
+/*******************************************************/
 % TESTAVIMAS
-/*******************************************************
+/*******************************************************/
 % 6 - 4 = 2
 %diff( s(s(s(s(s(s(0)))))), s(s(s(s(0)))), s(s(0)) ).
-/*******************************************************
+/*******************************************************/
 % 3 - 2 = 1
 % diff(s(s(s(0))), s(s(0)), s(0)).
-/*******************************************************
+/*******************************************************/
 % 3 - 2 = X
 % diff(s(s(s(0))), s(s(0)), X).
 
@@ -74,7 +82,9 @@ X = s(s(0))
       2    2  Exit: diff(s(s(0)),0,s(s(0))) ?
       1    1  Exit: diff(s(s(s(0))),s(0),s(s(0))) ?
 X = s(s(0)) 
-/*******************************************************
+*/
+
+/*******************************************************/
 % 1 - 1 = 0
 % diff(s(s(0)), s(s(0)), s(0)).
 
@@ -91,7 +101,9 @@ diff(1,1,0) ->
 diff(0,0,0) -> True
 
 O(n)
-/*******************************************************
+*/
+
+/*******************************************************/
 % 1 - 2 = 0
 % diff(s(0), s(s(0)), 0).
 /*
@@ -107,6 +119,7 @@ diff(1,2,0) ->
 diff(0,1,0) -> no
 
 O(n)
-/*******************************************************
+*/
+/*******************************************************/
 
 
---
+++ lab3.pl
@@ -0,0 +1,72 @@
+/*
+#################################### SALYGA  ########################################
+Apibrėžkite paskirtus keturis predikatus pagal nurodytas sąlygas. Nors 
+kai kurių užduočių formulavimas yra procedūrinis, reikia siekti, kiek įmanoma,
+ deklaratyvios programos. Nenaudokite Prolog darbo su sąrašais predikatų.
+#####################################################################################
+*/
+
+/*
+################################ UZDUOTIS 1  ########################################
+[1] Paprasti predikatai su sveikųjų skaičių sąrašais:
+
+[1.2] lyginiai(S) - teisingas, kai visi duoto sveikųjų skaičių sąrašo S elementai yra lyginiai. Pavyzdžiui:
+
+?- lyginiai([4,18,24,10]).
+true.
+#####################################################################################
+*/
+
+
+
+
+
+
+
+/*
+################################ UZDUOTIS 2  ########################################
+[2] Paprasti nearitmetiniai predikatai:
+
+[2.2] apjungti(SS,R) - sąrašas R gaunamas iš duotojo sąrašų sąrašo SS, sujungus pastarojo sąrašus į bendrą sąrašą. Giliuosius sąrašus apdoroti nėra būtina. Pavyzdžiui:
+
+?- apjungti([[a,b],[c],[d,[e,f], g]],R).
+R = [a,b,c,d,[e,f],g].
+#####################################################################################
+*/
+
+
+
+
+
+
+/*
+################################ UZDUOTIS 3  ########################################
+[3] Sudėtingesni predikatai:
+
+[3.1] ieina(S,R) - teisingas, kai visi duoto sąrašo S elementai įeina į sąrašą R. Pavyzdžiui:
+
+?- ieina([1,4],[3,2,4,1,5]).
+true.
+#####################################################################################
+*/
+
+
+
+
+
+
+
+/*
+################################ UZDUOTIS 4  ########################################
+[4] Operacijos su natūraliaisiais skaičiais, išreikštais skaitmenų sąrašais:
+
+[4.3] suma(S1,S2,Sum) - S1 ir S2 yra skaičiai vaizduojami skaitmenų sąrašais. Sum - tų skaičių suma vaizduojama skaitmenų sąrašu. Pavyzdžiui:
+
+?- suma([9,4,6,1,3,4],[2,8],Sum).
+Sum = [9,4,6,1,6,2].
+#####################################################################################
+*/
+
+
+
+
