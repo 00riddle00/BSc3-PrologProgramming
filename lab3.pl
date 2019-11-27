ef10dec2c839f4a093f9e867e2f975f39cf23c95
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2019-11-27 23:28:17 +0200

    .

--- lab3.pl
+++ lab3.pl
@@ -42,12 +42,32 @@ apjungti([[]], R) :- R = [].
 
 apjungti([[H1|[]],[H2|T2]], R) :- R = [H1,H2|T2].
 apjungti([[H1|T1],[H2|T2]], R) :- apjungti([T1, [H2|T2]], Ri), apjungti([[H1],Ri], R).
+apjungti([H|T],R) :- apjungti(T,Ri), apjungti([H,Ri], R), !.
+
+%2.2
+idek([A], [X|Y], R):- R = [A, X|Y].
+idek([A,B|C], [X|Y], R):- idek([B|C],[X|Y], RN), idek([A], RN, R).
+
+apjunk([],_R).
+apjunk([A,B], R):- idek(A,B,R), !.
+%apjunk([A,B,C], R):- idek(B,C,RN), idek(A,RN,R).
+apjunk([A,B,C|D], R):- idek(A,B,RN), apjunk([RN,C|D],R), !.
+
+
+sulieti([], S2, S1).
+sulieti(S1,[],S1).
+sulieti([G1|U1], [G2|U2], [G1|R]) :- sulieti(U1, [G2|U2], R), !.
+
+% apjungti([[a,b,c],[d,e,f]], R).
+% apjungti([[a,b,c],[d,e,f],[g,h,i]], R).
+% apjungti([[a,b,c],[d,e,f],[g,h,i],[j,k,l],[m,n,o]], R).
 
 
 
-
-
-
+% apjungti([[a,b],[c],[d,[e,f], g]],R).
+%
+join([], A, A).
+join([E|A], B, [E|AB]) :- join(A, B, AB).
 
 
 /*
@@ -71,7 +91,8 @@ true.
 ################################ UZDUOTIS 4  ########################################
 [4] Operacijos su natūraliaisiais skaičiais, išreikštais skaitmenų sąrašais:
 
-[4.3] suma(S1,S2,Sum) - S1 ir S2 yra skaičiai vaizduojami skaitmenų sąrašais. Sum - tų skaičių suma vaizduojama skaitmenų sąrašu. Pavyzdžiui:
+[4.3] suma(S1,S2,Sum) - S1 ir S2 yra skaičiai vaizduojami skaitmenų sąrašais. Sum - tų skaičių
+suma vaizduojama skaitmenų sąrašu. Pavyzdžiui:
 
 ?- suma([9,4,6,1,3,4],[2,8],Sum).
 Sum = [9,4,6,1,6,2].
