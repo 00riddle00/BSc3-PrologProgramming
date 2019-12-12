11ff10c2cc01e3dcf97921a7ea22a3d1a9e0cb02
Tomas Giedraitis <tomasgiedraitis@gmail.com>
2019-12-12 14:32:43 +0200

    .

--- lab3.pl
+++ lab3.pl
@@ -93,20 +93,22 @@ Sum = [9,4,6,1,6,2].
 
 sum_begin(A,B,C) :- sum(A,B,C,0).
 
-%sum(A,[],A,M).
-%sum([],B,B,M).
-%sum(A,B,C) :- suma(A,B,S1), removeLast(A, A1), removeLast(B, B1), sum(A1, B1, S2), C = [S2,S1].
+sum([],[],M).
+%sum([H],[],[H],0).
+%sum([],[H],[H],0).
+sum(A,[],A,0).
+sum([],A,A,0).
 sum(A,B,C,M) :- suma(A,B,S1,M,Mout), removeLast(A, A1), removeLast(B, B1), sum(A1, B1, S2, Mout), apjungti([S2, [S1]], C).
 
 suma([], [], S, M, Mout).
 
 suma([H], [], S, 1, Mout) :- SUM is H + 1, SUM >= 10, write('here2'), S is SUM - 10, Mout = 1.
 suma([H], [], S, 1, Mout) :- SUM is H + 1, S = SUM, Mout = 0.
-suma([H], [], S, 0, Mout).
+%suma([H], [], S, 0, Mout).
 
 suma([], [H], S, 1, Mout) :- SUM is H + 1, SUM >= 10, write('here2'), S is SUM - 10, Mout = 1.
 suma([], [H], S, 1, Mout) :- SUM is H + 1, S = SUM, Mout = 0.
-suma([], [H], S, 0, Mout).
+%suma([], [H], S, 0, Mout).
 
 suma([H|T], [], S, M, Mout) :- suma(T, [], S, M, Mout).
 suma([], [Hi|Ti], S, M, Mout) :- suma([], Ti, S, M, Mout).
@@ -117,16 +119,15 @@ suma([L], [Li], S, M, Mout) :- SUM is L + Li + M, S = SUM, Mout = 0.
 suma([H|T],[L], S, M, Mout) :- suma(T, [L], S, M, Mout).
 suma([L],[Hi|Ti], S, M, Mout) :- suma([L], Ti, S, M, Mout).
 
-%suma([H|T],[Hi|Ti], S) :- suma(T, Ti, Si). %, SUM is H + Hi, S = [SUM|Si].
 suma([H|T],[Hi|Ti], S, M, Mout) :- suma(T, Ti, S, M, Mout).
 
 removeLast([], []).
+%removeLast([], A).
 removeLast([H1], []).
 removeLast([H1,H2], [H1]).
 removeLast([H|T], RES) :- removeLast(T, RES1), RES = [H|RES1].
 
-%[2,3,4]
-%[2,5,8,3,4]
-%?- sum([2,3,4], [2,5,8,3,4], Sum).
 %?- sum_begin([2,3,4], [2,5,8,3,4], Sum).
-
+%?- sum_begin([2,5,8,3,4], [2,3,4], Sum).
+%?- sum_begin([1], [2,5,8,3,4], Sum).
+%?- sum_begin([1], [0,9,9], Sum).
